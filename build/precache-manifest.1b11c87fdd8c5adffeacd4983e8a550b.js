f0d316739f2d8b55bf80ac8a96939a51b5cb3e15
Dinesh kumar <67814205+dineshkumar12004@users.noreply.github.com>
2021-02-21 18:36:28 +0530

    uploaded files

---
+++ build/precache-manifest.1b11c87fdd8c5adffeacd4983e8a550b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb75d7e69f071c5bfcb1512cee2f99a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "884744937147d9c63ce7",
+    "url": "/static/css/main.0156f3b1.chunk.css"
+  },
+  {
+    "revision": "0d94ad1ff09e7338b631",
+    "url": "/static/js/2.cefd6d63.chunk.js"
+  },
+  {
+    "revision": "e4d980926fadc05888ff1db78303136f",
+    "url": "/static/js/2.cefd6d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884744937147d9c63ce7",
+    "url": "/static/js/main.389ddf4a.chunk.js"
+  },
+  {
+    "revision": "e6cda86ce7e8f7445663",
+    "url": "/static/js/runtime-main.dfbf61f0.js"
+  }
+]);
